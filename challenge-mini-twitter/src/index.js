eeffbe4f687b07711ca14c49a1400de3526a4ce9
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-08-12 12:52:58 +0100

    revised

--- challenge-mini-twitter/src/index.js
+++ challenge-mini-twitter/src/index.js
@@ -3,18 +3,40 @@ function newTwitt(e){
     let textArea = document.querySelector(".form-control");
     let parentElement = document.querySelector("#tweets");
     let theFirstChild = parentElement.firstChild;
-
-
     let newElement = document.createElement("p");
     
-   
-    parentElement.insertBefore(newElement, theFirstChild);
+
+    
     newElement.innerText = textArea.value;
     newElement.style.border = "solid black 3px";
     newElement.style.borderRadius = "5px";
     newElement.style.padding = "15px";
     
     newElement.style.width = "100%";
-    //newElement.style.height = "100px";
+    textArea.value = "";
+
+    let newTwitDeleteButton = document.createElement("button");
+    newTwitDeleteButton.className = "btn btn-secondary";
+    newTwitDeleteButton.style.border = "solid yellow 2px";
+    newTwitDeleteButton.style.padding = "5px";
+    newTwitDeleteButton.innerText= "X";
+    parentElement.insertBefore(newTwitDeleteButton,theFirstChild);
+    parentElement.insertBefore(newElement, newTwitDeleteButton);
+
 }
 document.querySelector(".btn-primary").addEventListener("click" ,newTwitt);
+
+
+
+let textArea = document.querySelector(".form-control");
+let enteredChar = document.querySelector(".entered-char");
+
+textArea.onkeyup = function () {
+  enteredChar.innerHTML = "The total characters entered are: " + this.value.length;
+  if (this.value.length > 280){
+      this.style.backgroundColor ="red";
+  }else{this.style.backgroundColor ="white"};
+
+};
+
+
