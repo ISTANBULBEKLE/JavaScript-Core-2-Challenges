c7804fa70c61370b14775d8b33ae6a26f70fa49d
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-08-12 14:33:20 +0100

    twitter q is revised

--- challenge-mini-twitter/src/index.js
+++ challenge-mini-twitter/src/index.js
@@ -7,21 +7,52 @@ function newTwitt(e){
     
 
     
-    newElement.innerText = textArea.value;
+    //newElement.innerText = textArea.value;
+    let innerHTML = textArea.value;
     newElement.style.border = "solid black 3px";
     newElement.style.borderRadius = "5px";
     newElement.style.padding = "15px";
+    newElement.style.display = "flex";
+    newElement.style.justifyContent = "space-between";
     
-    newElement.style.width = "100%";
+    newElement.style.width = "90%";
     textArea.value = "";
 
     let newTwitDeleteButton = document.createElement("button");
     newTwitDeleteButton.className = "btn btn-secondary";
+
+   /*  newTwitDeleteButton.setAttribute("type", "button");
+    newTwitDeleteButton.setAttribute("value", "Delete");
+    newTwitDeleteButton.setAttribute("id", "btnDelete"); */
+
+
     newTwitDeleteButton.style.border = "solid yellow 2px";
     newTwitDeleteButton.style.padding = "5px";
-    newTwitDeleteButton.innerText= "X";
-    parentElement.insertBefore(newTwitDeleteButton,theFirstChild);
-    parentElement.insertBefore(newElement, newTwitDeleteButton);
+    newTwitDeleteButton.innerText= "Delete";
+
+    
+    parentElement.insertBefore(newElement, theFirstChild);
+    
+
+    newTwitDeleteButton.addEventListener('click', function () {
+        this.parentElement.remove();
+    }, false); 
+    enteredChar.innerHTML = "The total characters entered are: ";
+
+    //if (textArea.value.includes(@)){
+
+        let tweet ="Hello world it is @SomeGuy and his friend @AnotherGuy, let's do it!";
+        let arr = tweet.split("@");
+        console.log(arr);
+        for(let i = 1; i < arr.length; i++){
+            let newArr = arr[i].split(" ");
+            let anotherArr = newArr[0].split(",");
+            let reg = /@ + anotherArr[0]/gi;
+            let templ1 = '<a href="www.twitter.com/' + anotherArr[0] + '">@' + anotherArr[0] + '</a>';
+            tweet.replace(reg,templ1);
+        }
+        newElement.innerHTML = 'Hello world it is < href="www.twitter.com/'+SomeGuy+'">@'+ SomeGuy +'</a>' +'and';newElement.appendChild(newTwitDeleteButton);
+
 
 }
 document.querySelector(".btn-primary").addEventListener("click" ,newTwitt);
